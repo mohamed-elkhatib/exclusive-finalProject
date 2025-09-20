4e503a39128867e833ed174eef7108b9ca131129
mohamed-elkhatib <khatibm2021@gmail.com>
2025-09-20 08:31:19 +0300

    change in token

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -1,10 +1,12 @@
-"use server"
+"use server";
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-
-  const encodedToken = (await cookies()).get("next-auth.session-token")?.value;
+  const cookieStore = await cookies();
+  const encodedToken =
+    cookieStore.get("next-auth.session-token")?.value ||
+    cookieStore.get("__Secure-next-auth.session-token")?.value;
   const decodedToken = await decode({
     token: encodedToken,
     secret: process.env.AUTH_SECRET!,
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,13 @@ import { NextResponse, NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
+  const cookieName=process.env.NODE_ENV ==="production"? "__Secure-next-auth.session-token":"next-auth.session-token"
+  const token = await getToken({ req: request,cookieName });
   const { pathname } = request.nextUrl;
+
   const authRoutes = ["/login", "/register "];
   const protectedRoutes = ["/cart", "/profile", "/checkout"];
+
   if (token && authRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL("/", request.url));
   }
